11bdbc8e8b99a413e039bd74c802cba2ba3fe581
Rodrigo Affonso <raffonso1978@gmail.com>
2023-03-18 09:10:15 -0300

    Tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,36 @@
 @extends('layouts.login')
 
 @section('content')
-<form class="form-signin" method="POST" action="{{ route('login.login') }}">
-    @csrf
-    <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Painel pessoal</h1>
-    <label for="email" class="sr-only">E-mail</label>
-    <input type="email" name="email" id="email" class="form-control" placeholder="E-mail" required autofocus>
-    <label for="password" class="sr-only">Senha</label>
-    <input type="password" name="password" id="password" class="form-control" placeholder="Senha" required>
-    {{-- <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div> --}}
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Autenticar</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-  </form>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="text-center">Login</h3>
+                </div>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login.login') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="email" name="email" class="form-control" id="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" id="password">
+                        </div>
+                        {{-- <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="remember">
+                            <label class="form-check-label" for="remember">Remember me</label>
+                        </div> --}}
+                        <button type="submit" class="btn btn-primary d-block w-100 mt-3">Login</button>
+                    </form>
+                </div>
+                {{--<div class="card-footer">
+                    <p class="text-center">Don't have an account? <a href="#">Register now</a></p>
+                </div> --}}
+            </div>
+        </div>
+    </div>
+</div>
   @endsection
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,26 +1,18 @@
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
-
-    <title>Signin Template for Bootstrap</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="/css/signin.css" rel="stylesheet">
-  </head>
-
-  <body class="text-center">
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+</head>
+<body>
     @yield('content')
-    
-  </body>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script> -->
+<script src="/js/bootstrap.min.js"></script>
+</body>
 </html>
+
+
